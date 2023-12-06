ad6bb5557e40f48696c042ff8c4a3851ff3ecc17
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-12-06 13:47:25 +0200

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,10 @@ export default function Document() {
 
       <Partytown debug={true} forward={["dataLayer.push"]} />
 
-      <Script
+      <script
         id="gtm"
         type="text/partytown"
-        strategy="beforeInteractive"
+        async={true}
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
