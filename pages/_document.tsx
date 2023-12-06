5a0754d1710d28629ef7f6dfaa2142315094d23e
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-12-06 11:27:29 +0200

    feat: add partytown

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,37 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Partytown } from "@builder.io/partytown/react";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Partytown debug={true} forward={['dataLayer.push']} />
+
+        <script
+          id="gtm"
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PXQZBDRL');`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PXQZBDRL"
+            height="0"
+            width="0"
+            className="hidden"
+          ></iframe>
+        </noscript>
       </body>
     </Html>
-  )
+  );
 }
