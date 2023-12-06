42a2077dca9c4e7c9d60b0db9fef7d2a16c7d829
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-12-06 11:43:39 +0200

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Partytown } from "@builder.io/partytown/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <Partytown debug={true} forward={["dataLayer.push"]} />
+
+        <script
+          id="gtm"
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PXQZBDRL');`,
+          }}
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
