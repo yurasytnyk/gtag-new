c3c6cf13f8a21e4d2264dc923e520cf249b75a27
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-12-06 13:31:54 +0200

    feat: updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,29 @@
 import { Partytown } from "@builder.io/partytown/react";
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Partytown debug={true} forward={["dataLayer.push"]} />
-      </Head>
+      <Head />
 
-      <body>
-        <Main />
-        <NextScript />
+      <Partytown debug={true} forward={["dataLayer.push"]} />
 
-        <script
-          id="gtm"
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Script
+        id="gtm"
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PXQZBDRL');`,
-          }}
-        />
+        }}
+      />
+
+      <body>
+        <Main />
+        <NextScript />
 
         <noscript>
           <iframe
