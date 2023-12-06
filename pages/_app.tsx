8e75e64d64f3915b682c601f8790a830a132ef4b
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-12-06 11:49:57 +0200

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Partytown debug={true} forward={["dataLayer.push"]} />
-
-      <script
-        id="gtm"
-        type="text/partytown"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PXQZBDRL');`,
-        }}
-      />
-
       <Component {...pageProps} />
     </>
   );
