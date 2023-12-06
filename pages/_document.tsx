ba27ea61cfbd2de9b17a559a3704ad325e66becc
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-12-06 11:34:31 +0200

    code update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,23 +5,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
 
-        <Partytown debug={true} forward={["dataLayer.push"]} />
+      <Partytown debug={true} forward={["dataLayer.push"]} />
 
-        <script
-          id="gtm"
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <script
+        id="gtm"
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PXQZBDRL');`,
-          }}
-        />
+        }}
+      />
+      
+      <body>
+        <Main />
+        <NextScript />
 
         <noscript>
           <iframe
